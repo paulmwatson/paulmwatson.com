f790a7a35416eef61d31acad0b1f46a159da5600
paulmwatson <github@paulmwatson.com>
2011-01-18 10:37:20 +0000

    Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pmw::Application.config.secret_token = '42c94ce0b82789a8bffcb09c9ecb7a7e5546a548564c129890e8a162695b7cc70ebf2807625836a7e83337c6639422e1028698aa60a74bc37274ae9701da1a6b'
