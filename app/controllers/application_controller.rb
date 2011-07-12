d47bbc926e4f6ce228e0a422b86c6ff345240b4a
Paul M. Watson <github@paulmwatson.com>
2011-07-12 23:22:03 +0100

    [+] added Intercomapp code and a simple user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,23 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   before_filter :set_locale
+  before_filter :set_user
 
   def set_locale
     I18n.locale = params[:locale]
+  end
+  
+  def set_user
+    if !session[:user]
+      user = User.create
+      user.email = "user_#{user.id}@paulmwatson.com"
+      user.save
+      session[:user] = user
+      @new_user = true
+    else
+      @new_user = false
+    end
+    @current_user = session[:user]
   end
   
 end
