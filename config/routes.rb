18368f76c7bae4edf0d6ce09cebf0a8ce0d19ba6
Paul M. Watson <github@paulmwatson.com>
2011-07-21 19:20:30 +0100

    [-] remove LinkedIn tests and some old routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,13 @@
 Pmw::Application.routes.draw do
+  # For STS135 launch 20110720
   get "/shuttle/" => 'site#shuttle'
   get "/shuttle/video/" => 'site#shuttle_video'
-  get "projects/index"
-
-  get "photos" => 'photos#index'
-  
-  get "litest" => 'site#linkedin'
 
   get "site/index"
   get "paulmwatson.vcf" => 'site#index', :format => :vcf
   get "vcard" => 'site#index', :format => :vcf
   
+  # Finished the internet for Mark Little 20110701
   match '/finished/' => 'site#finished'
 
   root :to => 'site#index'
